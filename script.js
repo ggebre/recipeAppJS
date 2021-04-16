36abc22fa6d96ab741b131d28c6e4e64d72c6b41
Getu Gebre <getu.gebre.gg@gmail.com>
2021-04-15 22:38:55 -0500

    recipe fetching and rendering

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const meals = document.getElementById('meals')
+const favMealContainer = document.getElementById('fav-meals')
+
 getRandomMeal()
+fetchFavMeals()
 async function getRandomMeal(){
     const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     const respData = await resp.json(); 
@@ -8,11 +11,15 @@ async function getRandomMeal(){
     addMeal(randomMeal, true)
 }
 async function getMealById(id){
-    const meal = await fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    const respData = await resp.json(); 
+    const meal = respData.meals[0] 
+    
+    return meal 
     
 }
 async function getMealBySearch(term){
-    const meals = await fetch("www.themealdb.com/api/json/v1/1/search.php?s=" + term)
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term)
     
 }
 function addMeal(mealData, random = false){
@@ -38,9 +45,52 @@ function addMeal(mealData, random = false){
         </button>
     </div>
     `
-    meal.querySelector('.meal-body .fav-btn')
-    .addEventListener('click', () => {
-        console.log("BUTTON CLICKED")
+    const btn = meal.querySelector('.meal-body .fav-btn')
+    
+    btn.addEventListener('click', (e) => {
+        if(btn.classList.contains('active')){
+            removeMealFromLS(mealData.idMeal)
+            btn.classList.remove('active')
+        }else{
+            addMealToLS(mealData.idMeal)
+            btn.classList.add('active')
+        }
+        favMealContainer.innerHTML = ""
+        fetchFavMeals()
     })
     meals.appendChild(meal)
+}
+function addFavMeals(mealData){
+    const favMeal = document.createElement('li')
+    favMeal.innerHTML = `  
+            <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
+            <spam>${mealData.strMeal}</spam>
+    `
+    favMealContainer.appendChild(favMeal)
+}
+// save in local storage 
+function addMealToLS(mealId){
+    const mealIds = getMealsFromS() 
+    localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]))
+}
+
+function removeMealFromLS(mealId){
+    const mealIds = getMealsFromS() 
+    localStorage.removeItem('mealIds', JSON.stringify(mealIds.filter(id => id !== mealId)))
+
+}
+function getMealsFromS(){
+    const mealIds = JSON.parse(localStorage.getItem('mealIds'))
+    
+    return mealIds === null ? [] : mealIds
+}
+
+async function fetchFavMeals(){
+    const mealIds = getMealsFromS()
+    for(let i=0; i < mealIds.length; i++){
+        const mealId = mealIds[i] 
+        meal = await getMealById(mealId) 
+    
+        addFavMeals(meal) 
+    }
 }
